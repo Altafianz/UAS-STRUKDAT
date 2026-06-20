1866b2e45dde91b0f5a4ebce30da5a7324fa6019
Fakhri <fakhialtafian010@gmail.com>
2026-06-20 13:15:36 +0700

    implementasi ui

--- window/mainwindow.cpp
+++ window/mainwindow.cpp
@@ -1,14 +1,34 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
+#include "editorwindow.h"
+#include "openfilewindow.h"
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    connect(ui->newFileButton, &QPushButton::clicked, this, &MainWindow::on_newFile_clicked);
+    connect(ui->openFileButton, &QPushButton::clicked, this, &MainWindow::on_openFile_clicked);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_newFile_clicked()
+{
+    EditorWindow *editor = new EditorWindow(this);
+    editor->show();
+    this->hide();
+}
+
+void MainWindow::on_openFile_clicked()
+{
+    OpenFileWindow *openFile = new OpenFileWindow(this);
+    openFile->show();
+    this->hide();
 }
--- window/openfilewindow.cpp
+++ window/openfilewindow.cpp
@@ -1,14 +1,25 @@
 #include "openfilewindow.h"
 #include "ui_openfilewindow.h"
+#include "mainwindow.h"
 
-OpenFileWindow::OpenFileWindow(QWidget *parent)
-    : QWidget(parent)
-    , ui(new Ui::OpenFileWindow)
+OpenFileWindow::OpenFileWindow(MainWindow *mainWin, QWidget *parent)
+    : QWidget(parent), ui(new Ui::OpenFileWindow), mainWindowRef(mainWin)
 {
     ui->setupUi(this);
+    this->setAttribute(Qt::WA_DeleteOnClose);
+
+    connect(ui->cancelButton, &QPushButton::clicked, this, &OpenFileWindow::on_backButton_clicked);
 }
 
 OpenFileWindow::~OpenFileWindow()
 {
     delete ui;
+}
+
+void OpenFileWindow::on_backButton_clicked()
+{
+    if (mainWindowRef) {
+        mainWindowRef->show();
+    }
+    this->close();
 }
